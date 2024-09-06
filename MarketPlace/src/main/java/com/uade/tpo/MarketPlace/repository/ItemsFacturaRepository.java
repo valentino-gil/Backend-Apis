628f1835a9b0b3b159ee5d8e77fe76e992082d85
GuidoRavaschio <guidoravaschio@gmail.com>
2024-09-06 18:44:35 -0300

    Creacion de los Repositorios Facturas e ItemsFacturas

---
+++ MarketPlace/src/main/java/com/uade/tpo/MarketPlace/repository/ItemsFacturaRepository.java
@@ -0,0 +1,19 @@
+package com.uade.tpo.MarketPlace.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.uade.tpo.MarketPlace.entity.Facturas;
+import com.uade.tpo.MarketPlace.entity.ItemsFactura;
+
+public interface ItemsFacturaRepository extends JpaRepository<Facturas,Long>{
+    
+    @Query("SELECT items FROM ItemsFactura items WHERE items.factura.id = :factura_id")
+    List<ItemsFactura> findAllItemsFactura(@Param("factura_id") Long factura_id);
+
+    @Query("SELECT item FROM ItemsFactura item WHERE item.id == :item_id")
+    ItemsFactura findItemFactura(@Param("item_id") Long id);
+}
